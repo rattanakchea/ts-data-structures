f5bac51aabc82a35c475f49212c92c70872914b2
Rattanak Chea <rattanak.chea@gmail.com>
2018-08-28 15:46:16 -0700

    added test, tsconfig with tsc --init

--- src/linked-list/linked-list.spec.ts
+++ src/linked-list/linked-list.spec.ts
@@ -9,3 +9,15 @@ console.log("----------");
 console.log("list2: " + list2);
 
 console.log("toArray: " + list2.toArray());
+
+beforeEach(() => {
+    list = new LinkedList<string>();
+  });
+  
+  test('initializes linked list', () => {
+    expect(list).toBeDefined();
+  });
+  
+  test('newly initialized linked list should have 0 length', () => {
+    expect(list.size()).toEqual(0);
+  });
