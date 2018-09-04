04754a4e08c4c17dbb2a9a1e3540ca9a5046f641
Rattanak Chea <rattanak.chea@gmail.com>
2018-09-04 15:39:30 -0700

    added sorting algorithms

---
+++ src/sorting-algorithms/merge-sort.ts
@@ -0,0 +1,13 @@
+// pseudo code
+/*
+- divide the array in half
+
+- merge sorted array
+*/
+
+/**
+ * description
+ * @param {Array} arr
+ * @return {Array} return sorted array
+ */
+function mergeSort<T>(arr: T[]) {}
